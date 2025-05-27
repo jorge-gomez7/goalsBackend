bf7b6bc9a17d7094880899e26954fa029a180997
Jorge Gómez <60752326+jorge-gomez7@users.noreply.github.com>
2025-05-26 21:27:28 -0600

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,6 @@
 module.exports = (req, res, next) => {
   const apiKey = req.headers['authorization'];
-  const myKey = '12345'; // Reemplaza con tu propia API key
+  const myKey = '12345'; 
 
   if (!apiKey || apiKey !== myKey) {
     return res.status(401).json({ error: 'Unauthorized' });
